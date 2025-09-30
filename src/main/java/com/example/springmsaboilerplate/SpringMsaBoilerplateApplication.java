131c63a95640ae6bfcef88d15b38f4f11aedf066
김한빈 <rnflrnfl41@naver.com>
2025-09-30 17:41:26 +0900

    initial commit

---
+++ src/main/java/com/example/springmsaboilerplate/SpringMsaBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmsaboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsaBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsaBoilerplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmsaboilerplate/SpringMsaBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmsaboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsaBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
