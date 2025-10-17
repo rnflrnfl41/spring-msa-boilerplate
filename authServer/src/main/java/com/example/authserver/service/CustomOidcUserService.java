b9b292733c63673ff5622af4c4f8a0662d4e06bb
김한빈 <rnflrnfl41@naver.com>
2025-10-17 14:32:46 +0900

    feat: Custom OidcUser Service

---
+++ authServer/src/main/java/com/example/authserver/service/CustomOidcUserService.java
@@ -0,0 +1,21 @@
+package com.example.authserver.service;
+
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOidcUserService extends OidcUserService {
+
+    @Override
+    public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+        OidcUser oidcUser = super.loadUser(userRequest);
+
+        // Google OIDC 사용자 정보를 그대로 사용
+        // 권한은 SecurityConfig에서 처리
+        
+        return oidcUser;
+    }
+}
